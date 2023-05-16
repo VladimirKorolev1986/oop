be1afd80222c4f3c309015fe22894952a827f461
VladimirKorolev <i@devkorolev.ru>
2023-05-16 20:23:27 +0300

    1.4 4

--- 1.4 4.py
+++ 1.4 4.py
@@ -1,7 +1,17 @@
 class MediaPlayer:
 
-    def open(self):
-        pass
+    def open(self, file):
+        self.filename = file
 
     def play(self):
-        pass
+        print(f'Воспроизведение {self.filename}')
+
+
+media1 = MediaPlayer()
+media2 = MediaPlayer()
+
+media1.open('filemedia1')
+media2.open('filemedia2')
+
+media1.play()
+media2.play()
