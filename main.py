7371cd87cec3e35a8ab83a3a07fc9e8f4a53c9ec
VladimirKorolev <i@devkorolev.ru>
2023-05-16 19:47:37 +0300

    1.3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Server:
-    ip = 1
+    ip = 0
 
     def __init__(self):
         self.ip += 1
@@ -7,7 +7,17 @@ class Server:
 
 
 class Router:
-    pass
+
+    @classmethod
+    def link(cls):
+        """для присоединения сервера server (объекта класса Server)
+        к роутеру (для простоты, каждый сервер соединен только с одним роутером"""
+        pass
+
+    @classmethod
+    def link(cls):
+        """для отсоединения сервера server (объекта класса Server) от роутера"""
+        pass
 
 
 class Data:
@@ -17,7 +27,3 @@ class Data:
 sv0 = Server()
 sv1 = Server()
 sv2 = Server()
-
-print(sv0.ip)
-print(sv0.ip)
-print(sv0.ip)
