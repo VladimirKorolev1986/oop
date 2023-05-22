2f2ea2bf8ca8d2dd9bb3ea0792d35a2011e1f6cf
VladimirKorolev <i@devkorolev.ru>
2023-05-22 21:49:50 +0300

    1.7 9

---
+++ 1.8 9.py
@@ -0,0 +1,36 @@
+class Video:
+
+    def __init__(self):
+        self.name = None
+
+    def create(self, name):
+        self.name = name
+        return self.name
+
+    def play(self):
+        print(f'воспроизведение видео {self.name}')
+
+
+class YouTube:
+    videos = []
+
+    @classmethod
+    def add_video(cls, video):
+        cls.videos.append(video)
+
+    @classmethod
+    def play(cls, video_indx):
+        return cls.videos[video_indx].play()
+
+
+v1 = Video()
+v2 = Video()
+
+v1.create("Python")
+v2.create("Python ООП")
+
+YouTube.add_video(v1)
+YouTube.add_video(v2)
+
+YouTube.play(0)
+YouTube.play(1)
