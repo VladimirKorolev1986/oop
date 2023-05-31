98c76de778792ec8daff60523e91c092c16dcbb8
VladimirKorolev <i@devkorolev.ru>
2023-05-31 19:15:24 +0300

    2.2 6

--- 2/2.2 6.py
+++ 2/2.2 6.py
@@ -17,36 +17,79 @@ class StackObj:
 
     @next.setter
     def next(self, obj):
-        if isinstance(self.__next, StackObj) or self.__next is None:
+        if isinstance(obj, StackObj) or obj is None:
             self.__next = obj
 
 
 class Stack:
     def __init__(self):
         self.top = None
+        self.last = None
 
     def push(self, obj):
         """добавление объекта класса StackObj в конец односвязного списка;"""
-        if not self.top:
-            self.top = obj
-        else:
 
+        if self.last:
+            self.last.next = obj
+        self.last = obj
+        if self.top is None:
+            self.top = obj
 
     def pop(self):
-        """извлечение последнего объекта с его удалением из односвязного списка;"""
-        pass
+        h = self.top
+        if h is None:
+            return
+        while h and h.next != self.last:
+            h = h.next
+        if h:
+            h.next = None
+        last = self.last
+        self.last = n
+        if self.last is None:
+            self.top = None
+        return last
 
     def get_data(self):
-        """получение списка из объектов односвязного списка
-        (список из строк локального атрибута __data каждого объекта в порядке их добавления,
-        или пустой список, если объектов нет)"""
-        pass
+        s = []
+        h = self.top
+        while h:
+            s.append(h.data)
+            h = h.next
+        return s
 
 
+s = Stack()
+top = StackObj("obj_1")
+s.push(top)
+s.push(StackObj("obj_2"))
+s.push(StackObj("obj_3"))
+s.pop()
 
-st = Stack()
-st.push(StackObj("obj1"))
-st.push(StackObj("obj2"))
-st.push(StackObj("obj3"))
-st.pop()
-res = st.get_data()    # ['obj1', 'obj2']
+res = s.get_data()
+assert res == ["obj_1", "obj_2"], f"метод get_data вернул неверные данные: {res}"
+assert s.top == top, "атрибут top объекта класса Stack содержит неверное значение"
+
+h = s.top
+while h:
+    res = h.data
+    h = h.next
+
+s = Stack()
+top = StackObj("obj_1")
+s.push(top)
+s.pop()
+assert s.get_data() == [], f"метод get_data вернул неверные данные: {s.get_data()}"
+
+n = 0
+h = s.top
+while h:
+    h = h.next
+    n += 1
+
+assert n == 0, "при удалении всех объектов, стек-подобная стурктура оказалась не пустой"
+
+s = Stack()
+top = StackObj("name_1")
+s.push(top)
+obj = s.pop()
+assert obj == top, "метод pop() должен возвращать удаляемый объект"
