a5aef1f582ce0cca3f51afcc03dbdb8e3c37002d
VladimirKorolev <i@devkorolev.ru>
2023-06-04 16:10:27 +0300

    2.3 8

---
+++ 2/2.3 8.py
@@ -0,0 +1,51 @@
+class StringValue:
+    def __init__(self, min_length=2, max_length=50):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if isinstance(value, str) and 2 <= len(value) <= 50:
+            return setattr(instance, self.name, value)
+
+
+class PriceValue:
+    def __init__(self, max_value=10_000):
+        self.max_value = max_value
+
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if isinstance(value, int) and 0 <= value <= 10000:
+            return setattr(instance, self.name, value)
+
+
+class SuperShop:
+    def __init__(self, name):
+        self.name = name
+        self.goods = []
+
+    def add_product(self, product):
+        self.goods.append(product)
+
+    def remove_product(self, product):
+        self.goods.remove(product)
+
+
+class Product:
+    name = StringValue()
+    price = PriceValue()
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
