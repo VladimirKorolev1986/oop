e11fef2cbdbf222ab637772bd2b2107eafa7f4a8
VladimirKorolev <i@devkorolev.ru>
2023-06-06 22:00:30 +0300

    3.1 6

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
-n = '5.3'
-print(type(n))
+class VK:
+    def __init__(self):
+        self.name = 'VK'
 
-if not isinstance(n, (int, float)):
-    print('yes')
-else:
-    print('not')
+
+class Youtube:
+    def __init__(self):
+        self.name = 'Youtube'
+
+
+class APP:
+    def __init__(self):
+        self.lst = []
+
+    def verify(self, obj):
+        if all(map(lambda x: type(x) == obj, self.lst)):
+            self.lst.append(obj)
