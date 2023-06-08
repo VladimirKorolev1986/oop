1571a8bd90cf08ff39106112d64a77dadc728330
VladimirKorolev <i@devkorolev.ru>
2023-06-08 20:47:17 +0300

    3.2 2

---
+++ 3/3.2 2.py
@@ -0,0 +1,27 @@
+import random
+
+
+class RandomPassword:
+    def __init__(self, psw_chars, min_length, max_length):
+        self.psw_chars = psw_chars
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __call__(self, *args, **kwargs):
+        pw = ''
+        for i in range(random.randint(self.min_length, self.max_length)):
+            pw += random.choice(self.psw_chars)
+        return pw
+
+
+min_length = 5
+max_length = 20
+psw_chars = "qwertyuiopasdfghjklzxcvbnm0123456789!@#$%&*"
+
+rnd = RandomPassword(psw_chars, min_length, max_length)
+
+psw = rnd()
+
+lst_pass = [rnd() for _ in range(3)]
+
+print(lst_pass)
--- test.py
+++ test.py
@@ -1,17 +1,3 @@
-class VK:
-    def __init__(self):
-        self.name = 'VK'
+import random
 
-
-class Youtube:
-    def __init__(self):
-        self.name = 'Youtube'
-
-
-class APP:
-    def __init__(self):
-        self.lst = []
-
-    def verify(self, obj):
-        if all(map(lambda x: type(x) == obj, self.lst)):
-            self.lst.append(obj)
+print(random.randint(5,20))
