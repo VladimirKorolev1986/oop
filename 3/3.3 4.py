0d5cbdcb8e5eacdf68841b1e7e1104b717234fee
VladimirKorolev <i@devkorolev.ru>
2023-06-12 11:40:48 +0300

    3.3 4

--- 3/3.3 4.py
+++ 3/3.3 4.py
@@ -1,2 +1,26 @@
 class WordString:
-   
+    def __init__(self, string=None):
+        self.__string = string
+
+    @property
+    def string(self):
+        return self.__string
+
+    @string.setter
+    def string(self, string):
+        self.__string = string
+
+    def __len__(self):
+        return len(self.string.split())
+
+    def __call__(self, indx, *args, **kwargs):
+        s = self.string.split()
+        return s[indx]
+
+
+words = WordString()
+words.string = "Курс по Python ООП"
+n = len(words)
+first = "" if n == 0 else words(0)
+print(words.string)
+print(f"Число слов: {n}; первое слово: {first}")
