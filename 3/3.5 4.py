72deb50dbefa4056a34d86a8ab9c8df5e41ce329
VladimirKorolev <i@devkorolev.ru>
2023-06-25 20:35:37 +0300

    3.5 4

---
+++ 3/3.5 4.py
@@ -0,0 +1,65 @@
+class Dimensions:
+    MIN_DIMENSION = 10
+    MAX_DIMENSION = 10000
+
+    def __init__(self, a, b, c):
+        self.__a = self.__b = self.__c = None
+        self.a = a
+        self.b = b
+        self.c = c
+
+    @property
+    def a(self):
+        return self.__a
+
+    @a.setter
+    def a(self, a):
+        if self.MIN_DIMENSION <= a <= self.MAX_DIMENSION:
+            self.__a = a
+
+    @property
+    def b(self):
+        return self.__b
+
+    @b.setter
+    def b(self, b):
+        if self.MIN_DIMENSION <= b <= self.MAX_DIMENSION:
+            self.__b = b
+
+    @property
+    def c(self):
+        return self.__c
+
+    @c.setter
+    def c(self, c):
+        if self.MIN_DIMENSION <= c <= self.MAX_DIMENSION:
+            self.__c = c
+
+    def dim(self):
+        return self.a * self.b * self.c
+
+    def __le__(self, other):
+        return self.dim() <= other.dim()
+
+    def __lt__(self, other):
+        return self.dim() < other.dim()
+
+
+class ShopItem:
+    def __init__(self, name, price, dim):
+        self.name = name
+        self.price = price
+        self.dim = dim
+
+
+trainers = ShopItem('кеды', 1024, Dimensions(40, 30, 120))
+umbrella = ShopItem('зонт', 500.24, Dimensions(10, 20, 50))
+fridge = ShopItem('холодильник', 40000, Dimensions(2000, 600, 500))
+chair = ShopItem('табуретка', 2000.99, Dimensions(500, 200, 200))
+lst_shop = (trainers, umbrella, fridge, chair)
+
+for i in lst_shop:
+    print(i.dim)
+
+lst_shop_sorted = sorted(lst_shop, key=lambda x: x.dim)
+
