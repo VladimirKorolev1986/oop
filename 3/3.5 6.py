2b25d87349a8f1332e91bc2c7715aa0903b92308
VladimirKorolev <i@devkorolev.ru>
2023-06-28 22:09:20 +0300

    3.5 7

--- 3/3.5 6.py
+++ 3/3.5 6.py
@@ -1,7 +1,35 @@
 class Morph:
     def __init__(self, *args):
-        self.word = args
+        self.words = list(map(lambda x: x.strip(',.!?:;').lower(), args))
+
+    def add_word(self, word):
+        w = word.lower()
+        if w not in self.words:
+            self.words.append(w)
+
+    def get_words(self):
+        return tuple(self.words)
+
+    def __eq__(self, other):
+        if type(other) != str:
+            raise ValueError("операнд должен быть строкой")
+        return other.lower() in self.words
 
 
-m = Morph('Vova1', 'Vova2', 'Vova3')
+dict_words = [Morph('связь', 'связи', 'связью', 'связи', 'связей', 'связям', 'связями', 'связях'),
+              Morph('формула', 'формулы', 'формуле', 'формулу', 'формулой', 'формул', 'формулам', 'формулами',
+                    'формулах'),
+              Morph('вектор', 'вектора', 'вектору', 'вектором', 'векторе', 'векторы', 'векторов', 'векторам',
+                    'векторами', 'векторах'
+                    ),
+              Morph('эффект', 'эффекта', 'эффекту', 'эффектом', 'эффекте', 'эффекты', 'эффектов', 'эффектам',
+                    'эффектами', 'эффектах'
+                    ), Morph('день', 'дня', 'дню', 'днем', 'дне', 'дни', 'дням', 'днями', 'днях'
+                             )]
 
+text = 'Мы будем устанавливать связь завтра днем.'
+
+words = map(lambda x: x.strip(',.!?:;').lower(), text.split())
+res = sum(word == morph for word in words for morph in dict_words)
+
+print(res)
