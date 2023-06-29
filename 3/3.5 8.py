4700118cbdb6bda907cd867d9bbc0831ad30a7b1
VladimirKorolev <i@devkorolev.ru>
2023-06-29 22:09:02 +0300

    3.5 8

---
+++ 3/3.5 8.py
@@ -0,0 +1,117 @@
+class CentralBank:
+    rates = {'rub': 72.5, 'dollar': 1.0, 'euro': 1.15}
+
+    def __new__(cls, *args, **kwargs):
+        return
+
+    @classmethod
+    def register(cls, money):
+        money.cb = cls
+
+
+class Money:
+    EPS = 0.1
+    type_money = None
+
+    def __init__(self, volume=0):
+        self.__volume = self.__cb = None
+        self.volume = volume
+        self.cb = None
+
+    @property
+    def volume(self):
+        return self.__volume
+
+    @volume.setter
+    def volume(self, obj):
+        self.__volume = obj
+
+    @property
+    def cb(self):
+        return self.__cb
+
+    @cb.setter
+    def cb(self, obj):
+        self.__cb = obj
+
+    def get_volumes(self, other):
+        if self.cb is None:
+            raise ValueError("Неизвестен курс валют.")
+
+        v1 = self.volume / self.cb.rates[self.type_money]
+        v2 = other.volume / other.cb.rates[other.type_money]
+        return v1, v2
+
+    def __eq__(self, other):
+        v1, v2 = self.get_volumes(other)
+        return abs(v1 - v2) < self.EPS
+
+    def __lt__(self, other):
+        v1, v2 = self.get_volumes(other)
+        return v1 < v2
+
+    def __le__(self, other):
+        v1, v2 = self.get_volumes(other)
+        return v1 <= v2
+
+
+class MoneyR(Money):
+    type_money = 'rub'
+
+
+class MoneyD(Money):
+    type_money = 'dollar'
+
+
+class MoneyE(Money):
+    type_money = 'euro'
+
+# class MoneyD:
+#     def __init__(self, volume=0):
+#         self.__volume = self.__cb = None
+#         self.volume = volume
+#         self.cb = None
+#
+#     @property
+#     def volume(self):
+#         return self.__volume
+#
+#     @volume.setter
+#     def volume(self, obj):
+#         self.__volume = obj
+#
+#     @property
+#     def cb(self):
+#         return self.__cb
+#
+#     @cb.setter
+#     def cb(self, obj):
+#         self.__cb = obj
+#
+#
+# class MoneyE:
+#     def __init__(self, volume=0):
+#         self.__volume = self.__cb = None
+#         self.volume = volume
+#         self.cb = None
+#
+#     @property
+#     def volume(self):
+#         return self.__volume
+#
+#     @volume.setter
+#     def volume(self, obj):
+#         self.__volume = obj
+#
+#     @property
+#     def cb(self):
+#         return self.__cb
+#
+#     @cb.setter
+#     def cb(self, obj):
+#         self.__cb = obj
+#
+#
+# rub = MoneyR()  # с нулевым балансом
+# dl = MoneyD(1501.25)  # с балансом в 1501.25 долларов
+# euro = MoneyE(100)  # с балансом в 100 евро
