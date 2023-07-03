8f8a2388a5fe57859c1eedc187cb7c010119a3ef
Vladimir <theing1986@gmail.com>
2023-07-03 21:57:10 +0300

    u

--- 3/3.6 6.py
+++ 3/3.6 6.py
@@ -1,3 +1,6 @@
+import re, sys
+
+
 class ShopItem:
     def __init__(self, name: str, weight, price):
         self.name = name
@@ -11,14 +14,40 @@ class ShopItem:
         return self.name.lower() == other.name.lower() and self.weight == other.weight and self.price == other.price
 
 
-lst_in = ['Системный блок: 1500 75890.56',
-          'Монитор Samsung: 2000 34000',
-          'Клавиатура: 200.44 545',
-          'Монитор Samsung: 2000 34000']
+lst_in = list(map(str.strip, sys.stdin.readlines()))
 
-new_s = []
-for i in lst_in:
-    new_s.append(i.split(":"))
+shop_items = {}
+for str_in in lst_in:
 
-print(new_s)
+    groups = re.search(r'^([^:]+):\s+([\d\.]+)\s+([\d\.]+)\s*$', str_in).groups()
+    item = ShopItem(groups[0], groups[1], groups[2])
+    total = 1
+    if item in shop_items:
+        shop_items[item][1] += 1
+    else:
+        shop_items[item] = [item, total]
 
+it1 = ShopItem('name', 10, 11)
+it2 = ShopItem('name', 10, 11)
+assert hash(it1) == hash(it2), "разные хеши у равных объектов"
+
+it2 = ShopItem('name', 10, 12)
+assert hash(it1) != hash(it2), "равные хеши у разных объектов"
+
+it2 = ShopItem('name', 11, 11)
+assert hash(it1) != hash(it2), "равные хеши у разных объектов"
+
+it2 = ShopItem('NAME', 10, 11)
+assert hash(it1) == hash(it2), "разные хеши у равных объектов"
+
+name = lst_in[0].split(':')
+for sp in shop_items.values():
+    assert isinstance(sp[0], ShopItem) and type(sp[
+                                                    1]) == int, "в значениях словаря shop_items первый элемент должен быть объектом класса ShopItem, а второй - целым числом"
+
+v = list(shop_items.values())
+if v[0][0].name.strip() == "Системный блок":
+    assert v[0][1] == 1 and v[1][1] == 2 and v[2][1] == 1 and len(v) == 3, "неверные значения в словаре shop_items"
+
+if v[0][0].name.strip() == "X-box":
+    assert v[0][1] == 2 and v[1][1] == 1 and v[2][1] == 2 and len(v) == 3, "неверные значения в словаре shop_items"
--- 3/3.6 7.py
+++ 3/3.6 7.py
@@ -8,6 +8,22 @@ class DataBase:
     def read(self, pk):
         pass
 
+
 class Record:
-    def __init__(self):
-        
+    pk = 0
+
+    def __init__(self, fio, descr, old):
+        self.fio = fio
+        self.descr = descr
+        self.old = old
+        self.pk = Record.pk + 1
+        Record.pk = self.pk
+
+
+rec1 = Record('vova', 'kind', '36')
+rec2 = Record('vasya', 'kind', '37')
+rec3 = Record('lina', 'kind', '36')
+
+print(rec1.pk)
+print(rec2.pk)
+print(rec3.pk)
